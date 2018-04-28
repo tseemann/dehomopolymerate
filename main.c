c1d82c1174437bea90e4d5e8379f62694f4ff256
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-28 10:04:34 +1000

    Bump to v0.3

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ KSEQ_INIT(gzFile, gzread)
 #define EXENAME "dehomopolymerate"
 #define GITHUB_URL "https://github.com/tseemann/dehomopolymerate"
 #define AUTHOR "Torsten Seemann"
-#define VERSION "0.2"
+#define VERSION "0.3"
 
 // warning: implicit declaration of function 'fileno' [-Wimplicit-function-declaration]
 int fileno(FILE *stream);
